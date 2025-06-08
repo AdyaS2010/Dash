f36f44ffe799876f0cad9cada3f72e31b8a018a2
Adya S <adya.sastry@gmail.com>
2025-06-08 16:04:32 -0500

    Create screen_game.dart

---
+++ screen_game.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class ScreenGame extends StatelessWidget {
+  const ScreenGame({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Game'),
+      ),
+      body: const Center(
+        child: Text('Press play to start'),
+      ),
+    );
+  }
+}
+

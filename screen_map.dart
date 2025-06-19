01039ba7fb70068cc8be95e2acfcf4d5b2833e5b
Adya S <adya.sastry@gmail.com>
2025-06-19 02:16:58 -0500

    Update screen_map.dart

--- screen_map.dart
+++ screen_map.dart
@@ -1,18 +1,16 @@
 import 'package:Dash/lesson_plan.dart';
+// import 'package:Dash/game.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    final screenWidth =  MediaQuery.of(context).size.width;
+    final screenWidth = MediaQuery.of(context).size.width;
     final colWidth = screenWidth * 0.8;
-
 
     return Scaffold(
       backgroundColor: const Color(0xFF210F04), // solid version of background
@@ -31,16 +29,13 @@ class MapScreen extends StatelessWidget {
                   return const CircularProgressIndicator(); // loading
                 }
 
-
                 if (!snapshot.hasData || !snapshot.data!.exists) {
                   return const Text("User data not found");
                 }
 
-
                 final userData = snapshot.data!.data() as Map<String, dynamic>;
                 final username = userData['username'] ?? 'Unknown';
                 final highScore = userData['highScore'] ?? 0;
-
 
                 return Container(
                   width: colWidth,
@@ -75,105 +70,114 @@ class MapScreen extends StatelessWidget {
               },
             ),
 
+            //levels
+            FutureBuilder(
+              future: Future.wait([
+                FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(FirebaseAuth.instance.currentUser?.uid)
+                    .get(),
+                FirebaseFirestore.instance.collection('levels').get(),
+              ]),
+              builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                }
 
-          //levels
-           FutureBuilder(
-            future: Future.wait([
-              FirebaseFirestore.instance
-                  .collection('users')
-                  .doc(FirebaseAuth.instance.currentUser?.uid)
-                  .get(),
-              FirebaseFirestore.instance.collection('levels').get(),
-            ]),
-            builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              }
+                if (!snapshot.hasData ||
+                    snapshot.data == null ||
+                    snapshot.data![0] == null ||
+                    snapshot.data![1] == null) {
+                  return const Text("Error loading levels");
+                }
 
+                final userDoc = snapshot.data![0] as DocumentSnapshot;
+                final levelDocs = snapshot.data![1] as QuerySnapshot;
 
-              if (!snapshot.hasData ||
-                  snapshot.data == null ||
-                  snapshot.data![0] == null ||
-                  snapshot.data![1] == null) {
-                return const Text("Error loading levels");
-              }
+                final levelsUnlocked =
+                    (userDoc['levelsUnlocked'] as Map<String, dynamic>).map(
+                      (key, value) => MapEntry(key, value as bool),
+                    );
 
+                return Column(
+                  children: levelDocs.docs.map((doc) {
+                    final data = doc.data() as Map<String, dynamic>;
+                    final levelName = doc.id;
+                    final unlocked = levelsUnlocked[levelName] ?? false;
+                    final color = unlocked
+                        ? const Color(0xFF3A9D5D)
+                        : const Color(0xFF888888);
+                    final textColor = unlocked ? Colors.white : Colors.black;
 
-              final userDoc = snapshot.data![0] as DocumentSnapshot;
-              final levelDocs = snapshot.data![1] as QuerySnapshot;
-
-
-              final levelsUnlocked =
-                  (userDoc['levelsUnlocked'] as Map<String, dynamic>).map(
-                (key, value) => MapEntry(key, value as bool),
-              );
-
-
-              return Column(
-                children: levelDocs.docs.map((doc) {
-                  final data = doc.data() as Map<String, dynamic>;
-                  final levelName = doc.id;
-                  final unlocked = levelsUnlocked[levelName] ?? false;
-                  final color = unlocked ? const Color(0xFF3A9D5D) : const Color(0xFF888888);
-                  final textColor = unlocked ? Colors.white : Colors.black;
-
-
-                  return GestureDetector(
-                    onTap: unlocked
-                        ? () {
-                            // make sure to navigate to actual game screen
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => const Level1LessonPage()));
-                            // Navigator.push(context, MaterialPageRoute(builder: (context) => const GameScreen()));
-                          }
-                        : null,
-                    child: Container(
-                      width: colWidth,
-                      padding: const EdgeInsets.all(10.0),
-                      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
-                      decoration: BoxDecoration(
-                        color: color,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Row(
-                        children: [
-                          Image.asset(
-                            'assets/images/planet.png',
-                            width: 30,
-                            height: 30,
-                          ),
-                          const SizedBox(width: 10),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  data['name'] ?? levelName,
-                                  style: TextStyle(fontSize: 12, color: textColor),
+                    return GestureDetector(
+                      onTap: unlocked
+                          ? () {
+                              // make sure to navigate to lesson plan, followed by actual game screen
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      const Level1LessonPage(),
                                 ),
-                                Text(
-                                  data['description'] ?? '',
-                                  style: TextStyle(fontSize: 10, color: textColor),
-                                ),
-                              ],
+                              );
+                              // Navigator.push(context, MaterialPageRoute(builder: (context) => const GameScreen()));
+                            }
+                          : null,
+                      child: Container(
+                        width: colWidth,
+                        padding: const EdgeInsets.all(10.0),
+                        margin: const EdgeInsets.symmetric(
+                          vertical: 5,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          color: color,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              'assets/images/planet.png',
+                              width: 30,
+                              height: 30,
                             ),
-                          ),
-                          Image.asset(
-                            unlocked
-                                ? 'assets/images/unlocked.png'
-                                : 'assets/images/locked.png',
-                            width: 30,
-                            height: 30,
-                          ),
-                        ],
+                            const SizedBox(width: 10),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    data['name'] ?? levelName,
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: textColor,
+                                    ),
+                                  ),
+                                  Text(
+                                    data['description'] ?? '',
+                                    style: TextStyle(
+                                      fontSize: 10,
+                                      color: textColor,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Image.asset(
+                              unlocked
+                                  ? 'assets/images/unlocked.png'
+                                  : 'assets/images/locked.png',
+                              width: 30,
+                              height: 30,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  );
-                }).toList(),
-              );
-            },
-          ),
-
-
+                    );
+                  }).toList(),
+                );
+              },
+            ),
           ],
         ),
       ),

09756ed4383cca3ba58fef12d27b12e07474556b
Adya S <adya.sastry@gmail.com>
2025-06-27 05:05:03 -0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 // import 'package: flame/flame.dart';
 import 'screen_map.dart';
 import 'game.dart';
@@ -17,6 +18,7 @@ import 'package:google_fonts/google_fonts.dart';
   licorice = Color.fromRGBO(33, 15, 4, 100),
   raisin black = Color.fromRGBO(36, 35, 37),
 */
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -36,14 +38,30 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xFF210F04),
         useMaterial3: true,
       ),
-      // home: const LogIn(),
-      initialRoute: '/',
+      home: const SplashScreen(), // Changed to show splash screen first
       routes: {
-        '/': (context) => const LogIn(),
+        '/login': (context) => const LogIn(),
         '/map': (context) => const MapScreen(),
         '/sign_up': (context) => const SignUp(),
-        '/level1_game': (context) => const GameScreen(),
+        '/game': (context) => const GameScreen(),
       },
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Image.asset('assets/images/splash.gif', fit: BoxFit.cover),
+      nextScreen: const LogIn(),
+      splashTransition: SplashTransition.fadeTransition,
+      backgroundColor: const Color(0xFF210F04), // matching app background
+      duration: 3300, // 3-5 seconds (adjust as needed)
+      splashIconSize:
+          double.infinity, // make the GIF fill the screen completely
     );
   }
 }
